2ef7430497939a33b660f5adbb0cd5444c61aa97
Thien Vo <vohuythien@utexas.edu>
2018-03-30 19:32:51 -0500

    Added mlab env mongo url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const dbUrl = 'mongodb://localhost:27017/TodoApp';
 //Change mongoose's promise to node's promise
 mongoose.Promise = global.Promise;
 //Connect to db. Will not do any queries till connection has been established
-mongoose.connect(dbUrl);
+mongoose.connect(process.env.MONGODB_URI || dbUrl);
 
 
 module.exports = {mongoose};
